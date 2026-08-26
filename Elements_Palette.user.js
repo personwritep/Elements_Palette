d06340310e31a95d3a038c9f6289b73342d1ab8a
Personwritep <89448434+personwritep@users.noreply.github.com>
2026-08-26 19:23:00 +0900

    Bump version to 6.0 and update match patterns
    Updated version number and match patterns for user script.

--- Elements_Palette.user.js
+++ Elements_Palette.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name        Elements Palette ⭐
 // @namespace        http://tampermonkey.net/
-// @version        5.9
+// @version        6.0
 // @description        編集枠に各種要素を自動記入するツール
 // @author        Ameba Blog User
-// @match        https://blog.ameba.jp/ucs/entry/srventry*
-// @exclude        https://blog.ameba.jp/ucs/entry/srventrylist.do*
+// @match        https://blog.ameba.jp/ucs/entry/srventryinsertinput.do*
+// @match        https://blog.ameba.jp/ucs/entry/srventryupdateinput.do*
 // @icon        https://www.google.com/s2/favicons?sz=64&domain=ameblo.jp
 // @grant        none
 // @updateURL        https://github.com/personwritep/Elements_Palette/raw/main/Elements_Palette.user.js
